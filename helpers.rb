175dd6c6cd52534a26410ba5edfba4fe556bc4b1
aldizh <aldizhupani@gmail.com>
2021-02-10 09:13:46 -0600

    Reafctor and sanity check for diffeent test cases

--- helpers.rb
+++ helpers.rb
@@ -17,13 +17,13 @@ def get_file_name
 end
 
 def get_criteria_from_user
-  puts "Please select one of the matching criteria: \n1 -> email address \n2 -> phone number \n3 -> both"
+  puts "Please select one of the matching criteria: \n1 -> email address \n2 -> phone number \n3 -> email or phone"
   print ": "
   criteria = gets.chomp.to_i
 
   while ![1, 2, 3].include?(criteria)
     puts "Sorry, invalid selection"
-    puts "Please select one of the matching criteria: \n1 -> email address \n2 -> phone number \n3 -> both"
+    puts "Please select one of the matching criteria: \n1 -> email address \n2 -> phone number \n3 -> email or phone"
     print ": "
     criteria = gets.chomp.to_i
   end
@@ -33,49 +33,96 @@ def get_criteria_from_user
   return criteria
 end
 
-# if unique return empty row, otherwise return matching row
-def check_uniqueness(criteria, row, headers, unique_emails, unique_phone_numbers)
-  uniq = false
-  case criteria
-  when 1 # email check
-    headers.each_with_index do |header, index|
-      if header.downcase.include?('email') && row[index-1] && row[index-1].size
-        personID = unique_emails[row[index-1]]
-        if personID # first occurrence that is not uniq
-          row.unshift(personID)
-          return row
-        end
-      end
-    end
-  when 2 # phone check
-    puts unique_phone_numbers
-    headers.each_with_index do |header, index|
-      if header.downcase.include?('phone') && row[index-1] && row[index-1].size
-        personID = unique_phone_numbers[parse_phone_text(row[index-1])]
-        if personID # first occurrence that is not uniq
-          row.unshift(personID)
-          return row
-        end
-      end
-    end
-  when 3 # email and phone check
-    headers.each_with_index do |header, index|
-      personID = nil
-      if header.downcase.include?('email') && row[index-1] && row[index-1].size
-        if unique_emails[row[index-1]]
-          personID = unique_emails[row[index-1]]
-        end
-      end
-      if header.downcase.include?('phone') && row[index-1] && row[index-1].size
-        if unique_phone_numbers[parse_phone_text(row[index-1])]
-          personID = unique_phone_numbers[parse_phone_text(row[index-1])]
-        end
-      end
-      if personID # first occurrence that is not uniq
-        row.unshift(personID)
-        return row
-      end
+def sync_email_hash(row, email_hash, lineno)
+  email1 = row[4] && row[4].size && row[4]
+  email2 = row[5] && row[5].size && row[5]
+  if email1 && !email_hash[email1]
+    if email2 && email_hash[email2]
+      email_hash[email1] = email_hash[email2]
+    else
+      email_hash[email1] = lineno
     end
   end
-  []
+
+  if email2 && !email_hash[email2]
+    if email1 && email_hash[email1]
+      email_hash[email2] = email_hash[email1]
+    else
+      email_hash[email2] = lineno
+    end
+  end
+  return email_hash
+end
+
+def sync_phone_hash(row, phone_hash, lineno)
+  phone1 = row[2] && row[2].size && parse_phone_text(row[2])
+  phone2 = row[3] && row[3].size && parse_phone_text(row[3])
+  if !phone_hash[phone1]
+    if phone2 && phone_hash[phone2]
+      phone_hash[phone1] = phone_hash[phone2]
+    else
+      phone_hash[phone1] = lineno
+    end
+  end
+
+  if !phone_hash[phone2]
+    if phone1 && phone_hash[phone1]
+      phone_hash[phone2] = phone_hash[phone1]
+    else
+      phone_hash[phone2] = lineno
+    end
+  end
+  return phone_hash
+end
+
+def sync_phone_or_email_hash(row, phone_or_email, lineno)
+  phone1 = row[2] && row[2].size && parse_phone_text(row[2])
+  phone2 = row[3] && row[3].size && parse_phone_text(row[3])
+  email1 = row[4]
+  email2 = row[5]
+  if phone1 && !phone_or_email[phone1]
+    if phone_or_email[phone2]
+      phone_or_email[phone1] = phone_or_email[phone2]
+    elsif phone_or_email[email1]
+      phone_or_email[phone1] = phone_or_email[email1]
+    elsif phone_or_email[email2]
+      phone_or_email[phone1] = phone_or_email[email2]
+    else
+      phone_or_email[phone1] = lineno
+    end
+  end
+  if phone2 && !phone_or_email[phone2]
+    if phone_or_email[phone1]
+      phone_or_email[phone2] = phone_or_email[phone1]
+    elsif phone_or_email[email1]
+      phone_or_email[phone2] = phone_or_email[email1]
+    elsif phone_or_email[email2]
+      phone_or_email[phone2] = phone_or_email[email2]
+    else
+      phone_or_email[phone2] = lineno
+    end
+  end
+  if email1 && !phone_or_email[email1]
+    if phone_or_email[phone1]
+      phone_or_email[email1] = phone_or_email[phone1]
+    elsif phone_or_email[phone2]
+      phone_or_email[email1] = phone_or_email[phone2]
+    elsif phone_or_email[email2]
+      phone_or_email[email1] = phone_or_email[email2]
+    else
+      phone_or_email[email1] = lineno
+    end
+  end
+  if email2 && !phone_or_email[email2]
+    if phone_or_email[phone1]
+      phone_or_email[email2] = phone_or_email[phone1]
+    elsif phone_or_email[phone2]
+      phone_or_email[email2] = phone_or_email[phone2]
+    elsif phone_or_email[email1]
+      phone_or_email[email2] = phone_or_email[email1]
+    else
+      phone_or_email[email2] = lineno
+    end
+  end
+  return phone_or_email
 end
--- init.rb
+++ init.rb
@@ -17,12 +17,11 @@ end
 
 matchingCriteria = get_criteria_from_user # user input for matching criteria 
 
-
 data = [] # final output
 headers = [] # header row
-phone_hash = {}
-email_hash = {}
-phone_or_email = {}
+phone_hash = {} # to mark unique phone occurences e.g: "5551236789": "3"  
+email_hash = {} # to mark unique email occurences e.g: "test@gmail.com": "10" 
+phone_or_email = {} # a combination of the above
 
 # input data
 csv_file = CSV.open("#{file_name}")
@@ -42,119 +41,41 @@ csv_file.each do |row|
         end
         presonId = email_hash[email]
       else
-        email1 = row[4] && row[4].size && row[4]
-        email2 = row[5] && row[5].size && row[5]
-        if email1 && !email_hash[email1]
-          if email2 && email_hash[email2]
-            email_hash[email1] = email_hash[email2]
-          else
-            email_hash[email1] = csv_file.lineno + 1
-          end
-        end
-    
-        if email2 && !email_hash[email2]
-          if email1 && email_hash[email1]
-            email_hash[email2] = email_hash[email1]
-          else
-            email_hash[email2] = csv_file.lineno + 1
-          end
-        end
-
-        presonId = email_hash[row[4]].to_i
+        email_hash = sync_email_hash(row, email_hash, csv_file.lineno + 1)
+        presonId = email_hash[row[4]]
       end
     when 2 # phone check
+      phone1 = row[2] && row[2].size && parse_phone_text(row[2])
       if (headers.size == 5)
-        phone1 = row[2] && row[2].size && parse_phone_text(row[2])
         if !phone_hash[phone1]
           phone_hash[phone1] = csv_file.lineno + 1
         end
         presonId = phone_hash[phone1]
       else
-        phone1 = row[2] && row[2].size && parse_phone_text(row[2])
-        phone2 = row[3] && row[3].size && parse_phone_text(row[3])
-        if !phone_hash[phone1]
-          if phone2 && phone_hash[phone2]
-            phone_hash[phone1] = phone_hash[phone2]
-          else
-            phone_hash[phone1] = csv_file.lineno + 1
-          end
-        end
-    
-        if !phone_hash[phone2]
-          if phone1 && phone_hash[phone1]
-            phone_hash[phone2] = phone_hash[phone1]
-          else
-            phone_hash[phone2] = csv_file.lineno + 1
-          end
-        end
-    
-        presonId = phone_hash[parse_phone_text(row[2])].to_i
+        phone_hash = sync_phone_hash(row, phone_hash, csv_file.lineno + 1)
+        presonId = phone_hash[phone1]
       end
-    when 3 # email and phone check
-      # TO BE Implmented
+    when 3 # email or phone check
       if (headers.size == 5)
         phone1 = row[2] && row[2].size && parse_phone_text(row[2])
-        email = row[3] && row[3].size && row[3]
-        if !phone_or_email[phone1] || !phone_or_email[email]
-          if (phone1 && phone_or_email[phone1])
-            phone_or_email[email] = phone_or_email[phone1]
-          elsif (email && phone_or_email[email])
+        email = row[3]
+        if !phone_or_email[phone1]
+          if phone_or_email[email]
             phone_or_email[phone1] = phone_or_email[email]
           else
             phone_or_email[phone1] = csv_file.lineno + 1
+          end
+        elsif !phone_or_email[email]
+          if phone_or_email[phone1]
+            phone_or_email[email] = phone_or_email[phone1]
+          else
             phone_or_email[email] = csv_file.lineno + 1
           end
         end
-      else 
+        presonId = phone_or_email[phone1]
+      else
         phone1 = row[2] && row[2].size && parse_phone_text(row[2])
-        phone2 = row[3] && row[3].size && parse_phone_text(row[3])
-        email1 = row[4]
-        email2 = row[5]
-        if phone1 && !phone_or_email[phone1]
-          if phone_or_email[phone2]
-            phone_or_email[phone1] = phone_or_email[phone2]
-          elsif phone_or_email[email1]
-            phone_or_email[phone1] = phone_or_email[email1]
-          elsif phone_or_email[email2]
-            phone_or_email[phone1] = phone_or_email[email2]
-          else
-            phone_or_email[phone1] = csv_file.lineno + 1
-          end
-        end
-        if phone2 && !phone_or_email[phone2]
-          if phone_or_email[phone1]
-            phone_or_email[phone2] = phone_or_email[phone1]
-          elsif phone_or_email[email1]
-            phone_or_email[phone2] = phone_or_email[email1]
-          elsif phone_or_email[email2]
-            phone_or_email[phone2] = phone_or_email[email2]
-          else
-            phone_or_email[phone2] = csv_file.lineno + 1
-          end
-        end
-        if email1 && !phone_or_email[email1]
-          if phone1 && phone_or_email[phone1]
-            phone_or_email[email1] = phone_or_email[phone1]
-          elsif phone2 && phone_or_email[phone2]
-            phone_or_email[email1] = phone_or_email[phone2]
-          elsif email2 && phone_or_email[email2]
-            phone_or_email[email1] = phone_or_email[email2]
-          else
-            phone_or_email[email1] = csv_file.lineno + 1
-          end
-        end
-        if email2 && !phone_or_email[email2]
-          if phone_or_email[phone1]
-            phone_or_email[email2] = phone_or_email[phone1]
-          elsif phone_or_email[phone2]
-            phone_or_email[email2] = phone_or_email[phone2]
-          elsif phone_or_email[email1]
-            phone_or_email[email2] = phone_or_email[email1]
-          else
-            phone_or_email[email2] = csv_file.lineno + 1
-          end
-        end
-        puts phone_or_email
+        phone_or_email = sync_phone_or_email_hash(row, phone_or_email, csv_file.lineno + 1)
         presonId = phone_or_email[phone1]
       end
     end
